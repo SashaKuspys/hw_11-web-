c2868edc4948b11710d9d263288065b44401a2d3
SashaKuspys <134163412+SashaKuspys@users.noreply.github.com>
2023-12-28 18:38:22 +0200

    hw_12_web

--- src/schemas/contact.py
+++ src/schemas/contact.py
@@ -1,6 +1,9 @@
+from datetime import datetime
 from typing import Optional
 
 from pydantic import BaseModel, EmailStr, Field
+
+from src.schemas.user import UserResponse
 
 
 class ContactSchema(BaseModel):
@@ -24,6 +27,10 @@ class ContactResponse(BaseModel):
     phone: str
     birthday: str
     completed: bool
+    created_at: datetime | None
+    updated_at: datetime | None
+    user: UserResponse | None
+
 
     class Config:
         from_attributes = True
